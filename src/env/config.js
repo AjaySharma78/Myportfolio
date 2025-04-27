1ba6e9262ee03e76cc25268a413b8e14cce51cbd
Ajay sharma <aakirtsharma123@gmail.com>
2025-04-27 19:13:50 +0545

    new commit

--- src/env/config.js
+++ src/env/config.js
@@ -2,6 +2,7 @@ const config = {
     email: String(import.meta.env.VITE_EMAIL),
     whatsapp: String(import.meta.env.VITE_WHATSAPP),
     github: String(import.meta.env.VITE_GITHUB),
+    n8nLink: String(import.meta.env.VITE_N8N)
 };
 
 export default config;
