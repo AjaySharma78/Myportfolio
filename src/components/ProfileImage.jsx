7d626984376dd81abad3b17966d19cc48be689c8
Ajay sharma <aakirtsharma123@gmail.com>
2026-02-13 16:37:56 +0545

    Update image source in ProfileImage component

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function ProfileImage({ className = "" }) {
   return (
     <div className={`${className}`}>
-      <img src='/profile.jpg' alt="Logo" className={`${className}`} />
+      <img src='https://zykx2x7m0daf6gdp.public.blob.vercel-storage.com/profile.jpg' alt="Logo" className={`${className}`} />
     </div>
   );
 }
